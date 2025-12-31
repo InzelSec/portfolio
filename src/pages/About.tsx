2a4348587bb79c14abaa7fd320e5c204040eb647
InzelSec <144255240+InzelSec@users.noreply.github.com>
2025-12-31 14:22:55 -0300

    fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -38,11 +38,12 @@ const contactLinks = [
 ];
 
 const languages = [
-  { name: "Portuguese", namePt: "Português", level: "Native", levelPt: "Nativo" },
-  { name: "English", namePt: "Inglês", level: "Advanced/Fluent", levelPt: "Avançado-Fluente" },
+  { name: "English", namePt: "Inglês", level: "Advanced/Fluent", levelPt: "Avançado/Fluente" },
   { name: "Spanish", namePt: "Espanhol", level: "Basic-Intermediate", levelPt: "Básico-Intermediário" },
   { name: "German", namePt: "Alemão", level: "Basic-Intermediate", levelPt: "Básico-Intermediário" },
   { name: "Russian", namePt: "Russo", level: "Basic", levelPt: "Básico" },
+  { name: "Portuguese", namePt: "Português", level: "Native", levelPt: "Nativo" },
+
 ];
 
 const certifications = [
